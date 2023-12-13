03326601b20b407a1173554f5f5bfef91f04b212
747Socker <ygj960518@gmail.com>
2023-12-13 20:28:55 +0900

    🔨 fix(week02): 연구소 2

--- 747Socker/week02/연구소2.java
+++ 747Socker/week02/연구소2.java
@@ -7,95 +7,66 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class bj_bfs_연구소 {
-	
+
 	static class Point{
 		int x;
 		int y;
 		public Point(int x, int y) {
 			this.x = x;
 			this.y = y;
-			
+
 		}
 	}
-	
+
 	static int[][] map;
 	static int N;
 	static int M;
 	static int[] dx = { -1, 0, 1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
-	static int[][] chk;
-	static int[] pm;
-	static int[] ch;
-	static int[] arr;
-	static int nn;
+	static Point[] selected;
 	static ArrayList<Point> pts;
-	static ArrayList<Point> qs;
 	static Queue<Point> Q;
-	static int cnt;
-	static int max = Integer.MIN_VALUE;
-	static int[][] cMap;
-	
-	static void permu(int L) {
-		if(L==3) {
-			wall(pm);
-		}else {
-			for(int i=0; i<nn;i++) {
-				if(ch[i]==0) {
-					ch[i]=1;
-					pm[L]=arr[i];
-					permu(L+1);
-					ch[i]=0;
+	static int max = Integer.MAX_VALUE;
+
+	static boolean chk2(int[][] chk) {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(map[i][j]!=1 && chk[i][j]==0) {
+					return false;
 				}
 			}
 		}
+		return true;
 	}
-	
-	static void wall(int[] aa) {
-		for(int i=0; i<3; i++) {
-			Point tmp = pts.get(aa[i]-1);
-			cMap[tmp.x][tmp.y]=1;
-		}
-		bfs();
-		// re
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				cMap[i][j]=map[i][j];
-			}
-		}
-	}
-	static void chk2() {
-		cnt = 0;
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(cMap[i][j]==0) {
-					cnt++;
-				}
-			}
-		}
-		max = Math.max(max, cnt);
-	}
-	
+
 	static void bfs() {
-		chk = new int[N][M];
+		int[][] chk = new int[N][N];
 		Q = new LinkedList<Point>();
-		for(int i=0; i<qs.size();i++) {
-			Q.add(qs.get(i));
-			chk[qs.get(i).x][qs.get(i).y]=1;
+		int res = 0;
+		for(int i=0; i<M;i++) {
+			Q.add(selected[i]);
+			chk[selected[i].x][selected[i].y]=1;
 		}
-		
+
 		while(!Q.isEmpty()) {
-			Point tmp = Q.poll();
-			for(int d=0 ;d<4; d++) {
-				int nx = tmp.x + dx[d];
-				int ny = tmp.y + dy[d];
-				if(nx>=0 && nx<N && ny>=0 && ny<M && cMap[nx][ny]==0 && chk[nx][ny]==0) {
-					cMap[nx][ny]=2;
-					chk[nx][ny]=1;
-					Q.offer(new Point(nx,ny));
+			if(res>=max) return;
+			int size = Q.size();
+			for(int i=0; i<size; i++) {
+				Point tmp = Q.poll();
+				for(int d=0 ;d<4; d++) {
+					int nx = tmp.x + dx[d];
+					int ny = tmp.y + dy[d];
+					if(nx>=0 && nx<N && ny>=0 && ny<N && chk[nx][ny]==0) {
+						if(map[nx][ny]!=1) {
+							chk[nx][ny]=1;
+							Q.offer(new Point(nx,ny));
+						}
+					}
 				}
 			}
+			res++;
 		}
-		chk2();
+		if(chk2(chk)) max = Math.min(max, res-1);
 	}
 
 	public static void main(String[] args) {
@@ -103,37 +74,28 @@ public class bj_bfs_연구소 {
 		N = sc.nextInt();
 		M = sc.nextInt();
 		pts = new ArrayList<Point>();
-		map = new int[N][M];
+		map = new int[N][N];
 		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
+			for(int j=0; j<N; j++) {
 				map[i][j]=sc.nextInt();
-			}
-		}
-		
-		cMap = new int[N][M];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				cMap[i][j] = map[i][j];
-			}
-		}
-		
-		qs = new ArrayList<Point>();
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(map[i][j]==0) {
-					pts.add(new Point(i,j));
-				}else if(map[i][j]==2) {
-					qs.add(new Point(i,j));
+				if(map[i][j]==2) {
+					pts.add(new Point(i, j));
 				}
 			}
 		}
-		
-		nn = pts.size();
-		ch = new int[nn];
-		pm = new int[3];
-		arr=new int[nn];
-		Arrays.setAll(arr, num->num+1);
-		permu(0);
-		System.out.println(max);
+		selected = new Point[M];
+		sel(0,0);
+		if(max==Integer.MAX_VALUE) System.out.println(-1);
+		else System.out.println(max);
+	}
+	static void sel(int cnt, int s) {
+		if(cnt==M) {
+			bfs();
+			return;
+		}
+		for(int i=s; i<pts.size();i++) {
+			selected[cnt] = pts.get(i);
+			sel(cnt+1,i+1);
+		}
 	}
 }
