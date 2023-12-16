b19a39d7b97e4af277eeb61999853ab5269eb444
nijesmik <bkybb@duck.com>
2023-12-16 14:54:27 +0900

    ♻️ refactor(week03): 낚시왕

--- nijesmik/week03/낚시왕.java
+++ nijesmik/week03/낚시왕.java
@@ -1,4 +1,5 @@
 package nijesmik.week03;
+
 import java.util.Scanner;
 
 public class 낚시왕 {
@@ -10,17 +11,17 @@ public class 낚시왕 {
         R = sc.nextInt();
         C = sc.nextInt();
         M = sc.nextInt();
-        sea = new Shark[R+1][C+1];
+        sea = new Shark[R][C];
         for (int i = 0; i < M; i++) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
             int speed = sc.nextInt();
             int dir = sc.nextInt();
             int size = sc.nextInt();
             add(new Shark(r, c, speed, dir, size));
         }
         int ans = 0;
-        for (int i = 1; i <= C; i++) {
+        for (int i = 0; i < C; i++) {
             ans += fishing(i);
             move();
         }
@@ -29,9 +30,9 @@ public class 낚시왕 {
 
     static void move() {
         Shark[][] old = sea;
-        sea = new Shark[R+1][C+1];
-        for (int i = 1; i <= R; i++) {
-            for (int j = 1; j <= C; j++) {
+        sea = new Shark[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
                 if (old[i][j] != null) {
                     old[i][j].move();
                     add(old[i][j]);
@@ -57,10 +58,10 @@ public class 낚시왕 {
             int time = speed;
             int nr = r, nc = c;
             while (time-- > 0) {
-                if (nr == 1 && dir == 1) dir = 2;
-                else if (nr == R && dir == 2) dir = 1;
-                else if (nc == C && dir == 3) dir = 4;
-                else if (nc == 1 && dir == 4) dir = 3;
+                if (nr == 0 && dir == 1) dir = 2;
+                else if (nr == R-1 && dir == 2) dir = 1;
+                else if (nc == C-1 && dir == 3) dir = 4;
+                else if (nc == 0 && dir == 4) dir = 3;
                 nr = nr + dr[dir];
                 nc = nc + dc[dir];
             }
@@ -70,7 +71,7 @@ public class 낚시왕 {
     }
 
     static int fishing(int c) {
-        for (int i = 1; i <= R; i++) {
+        for (int i = 0; i < R; i++) {
             if (sea[i][c] != null) {
                 Shark shark = sea[i][c];
                 sea[i][c] = null;
