1922c167b9a76adef6b36634796bad9e84e69d0e
KimJihwan98 <wlghks98k@gmail.com>
2023-12-25 16:09:46 +0900

    🔨 fix(week03): 낚시왕

--- KimJihwan98/week03/낚시왕.java
+++ KimJihwan98/week03/낚시왕.java
@@ -13,7 +13,7 @@ class Shark {
 	int s = 0;
 	int d = 0;
 	int z = 0;
-	Shark() {};
+
 	Shark(int r, int c, int s, int d, int z) {
 		this.r = r;
 		this.c = c;
@@ -27,100 +27,93 @@ public class 낚시왕 {
 	static int R, C, M;
 	static Shark[][] map;
 	static int answer = 0;
-	static PriorityQueue<Shark> pq;
+
 	public static void main(String[] args) throws IOException {
-		//입력 받기 
+		// 입력 받기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		R = Integer.parseInt(st.nextToken()); 
+		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
 		map = new Shark[R][C];
-		pq = new PriorityQueue<>((s1, s2) -> s1.r-s2.r);
-		for(int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken()); 
+			int d = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			
-			map[r-1][c-1] = new Shark(r-1, c-1, s, d-1, z);
-			pq.add(map[r-1][c-1]);
+
+			map[r - 1][c - 1] = new Shark(r - 1, c - 1, s, d-1, z);
 		}
-		
-		bfs();
+
+		for (int i = 0; i < C; i++) {
+			fishing(i);
+			map = moveShark();
+		}
+
 		System.out.println(answer);
 	}
-	
-	static int[] dr = {-1, 1, 0, 0}; // 위 아래 오른쪽 왼쪽
-	static int[] dc = {0, 0, 1, -1};
-	static void bfs() {
-		PriorityQueue<Shark> pq2 = new PriorityQueue<>(new Comparator<Shark>() {
-			@Override
-			public int compare(Shark s1, Shark s2) {
-				if(s1.r == s2.r) {
-					if(s1.c == s1.c) {
-						return s2.z - s1.z;
-					}
-					return s1.c - s2.c;
-				}
-				return s1.r - s2.r;
-			}
-		}); //이거는 map을 초기화 할 때 사이즈가 큰 것만 초기화할라고.
-		
-		int human = 0;
-		boolean check = false;
-		while(human != R && M!=0) {
-			human++;
-			for(int i = 0; i < M; i++) {
-				Shark now = pq.poll();
-				if(human == now.c && !check) {
-					check = true;					//상어 한마리 잡았나 확인
-					answer += now.z;
-					map[now.r][now.c] = new Shark(); 
-				} else {
-					Shark tmp = moveShark(now);
-					map[tmp.r][tmp.c] = new Shark();
-					pq2.add(tmp);
-				}
-			}
-			M = M-1; // 상어 한마리 잡았으니까 상어 숫자 줄이기
-			
-			// 상어 움직임 토대로 map 바꾸기
-			while(!pq2.isEmpty()) {
-				Shark now = pq2.poll();
-				if(map[now.r][now.c].z != 0) { // 그 자리에 상어가 있으면
-					M--; //상어 총 마리 수 줄이기
-					continue;
-				} else {
-					map[now.r][now.c]= now;
-					pq.add(now);
-				}
-			}
+
+	private static void fishing(int column) {
+		for (int i = 0; i < R; i++) {
+			if (map[i][column] == null)
+				continue;
+
+			answer += map[i][column].z;
+			map[i][column] = null;
+			return;
 		}
 	}
-	static Shark moveShark(Shark shark) {
-		int dir = shark.d;
-		int speed = shark.s;
-		if(dir == 0 || dir == 1) {
-			while(speed != 0) {
-				if(shark.c == 0 && dir == 0) dir += 1;
-				if(shark.c == C-1 && dir == 1) dir -= 1;
-				shark.c += dc[dir];
-				speed--;
-			}
-		} else if(dir == 2 || dir == 3) {
-			while(speed != 0) {
-				if(shark.r == 0 && dir == 3) dir -= 1;
-				if(shark.r == R-1 && dir == 2) dir += 1;
-				shark.r += dr[dir];
-				speed--;
+
+	static int[] dr = { -1, 1, 0, 0 }; // 위 아래 오른쪽 왼쪽
+	static int[] dc = { 0, 0, 1, -1 };
+
+	private static Shark[][] moveShark() {
+		Shark[][] tmp = new Shark[R][C];
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] == null)
+					continue;
+
+				Shark now = map[i][j];
+				int speed = now.s;
+				if (now.d == 0 || now.d == 1)
+					speed %= (R - 1) * 2;
+				else
+					speed %= (C - 1) * 2;
+
+				for (int s = 0; s < speed; s++) {
+					int nr = now.r + dr[now.d];
+					int nc = now.c + dc[now.d];
+
+					if (nr < 0 || nc < 0 || R <= nr || C <= nc) {
+						now.r -= dr[now.d];
+						now.c -= dc[now.d];
+
+						if (now.d == 0)
+							now.d = 1;
+						else if (now.d == 1)
+							now.d = 0;
+						else if (now.d == 2)
+							now.d = 3;
+						else
+							now.d = 2;
+						continue;
+					}
+
+					now.r = nr;
+					now.c = nc;
+				}
+
+				if (tmp[now.r][now.c] != null && tmp[now.r][now.c].z > now.z)
+					continue;
+
+				tmp[now.r][now.c] = now;
 			}
 		}
-		shark.d = dir;
-		return shark;
+		return tmp;
 	}
 }
