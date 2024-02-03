5a81753837edab4b61ba418c5adc9630809bece2
nijesmik <bkybb@duck.com>
2024-02-03 21:51:04 +0900

    🔨 fix(week09): 도넛과 막대 그래프

--- nijesmik/week09/도넛과_막대_그래프/Solution.java
+++ nijesmik/week09/도넛과_막대_그래프/Solution.java
@@ -6,7 +6,6 @@ class Solution {
     int N = 1_000_000;
     Node[] nodes = new Node[N + 1];
     int start, ans[];
-    boolean[] visited = new boolean[N + 1];
 
     public int[] solution(int[][] edges) {
         for (int i = 1; i <= N; i++) {
@@ -14,13 +13,20 @@ class Solution {
         }
 
         List<Integer> starts = new ArrayList<>();
+        Set<Integer> ends = new HashSet<>();
         for (int[] edge : edges) {
             int idx = edge[0];
-            nodes[idx].next.add(edge[1]);
+            nodes[idx].next++;
             nodes[edge[1]].prev++;
-            if (nodes[idx].next.size() == 2) {
+            ends.add(edge[1]);
+            if (nodes[idx].next == 2) {
                 starts.add(idx);
-                visited[idx] = true;
+            }
+        }
+        ans = new int[4];
+        for (int idx : ends) {
+            if (nodes[idx].next == 0) {
+                ans[2]++;
             }
         }
         for (int idx : starts) {
@@ -28,46 +34,14 @@ class Solution {
                 start = idx;
             }
         }
-
-        ans = new int[4];
         ans[0] = start;
-        for (int node : nodes[start].next) {
-            s = node;
-            e = 0;
-            n = 1;
-            visited[node] = true;
-            dfs(node);
-            if (n == e) {
-                ans[1]++;
-            } else if (n - 1 == e) {
-                ans[2]++;
-            } else {
-                ans[3]++;
-            }
-        }
+        ans[3] = starts.size() - 1;
+        ans[1] = nodes[start].next - ans[3] - ans[2];
 
         return ans;
     }
 
-    int s, n, e;
-
-    void dfs(int cur) {
-        if (n < e) {
-            return;
-        }
-        while (!nodes[cur].next.isEmpty()) {
-            int idx = nodes[cur].next.pop();
-            if (!visited[idx]) {
-                visited[idx] = true;
-                n++;
-            }
-            e++;
-            dfs(idx);
-        }
-    }
-
     class Node {
-        Stack<Integer> next = new Stack<>();
-        int prev = 0;
+        int next, prev;
     }
 }
