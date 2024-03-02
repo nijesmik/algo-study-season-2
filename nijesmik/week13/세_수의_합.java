2637d55b389d056e9d750794e0f9945d1d38fbb9
nijesmik <bkybb@duck.com>
2024-03-03 00:34:14 +0900

    ✨ feat(week13): 세 수의 합

---
+++ nijesmik/week13/세_수의_합.java
@@ -0,0 +1,27 @@
+package nijesmik.week13;
+import java.util.*;
+
+public class 세_수의_합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> set = new TreeSet<>((a, b) -> b - a);
+        while (n-- > 0) {
+            set.add(sc.nextInt());
+        }
+        Set<Integer> sum = new HashSet<>();
+        for (int i : set) {
+            for (int j : set) {
+                sum.add(i + j);
+            }
+        }
+        for (int i : set) {
+            for (int j : set) {
+                if (sum.contains(i - j)) {
+                    System.out.println(i);
+                    return;
+                }
+            }
+        }
+    }
+}
