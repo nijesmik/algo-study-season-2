beacaa7ff958ecd2b57ff2d7b5a21234b2d804c2
747Socker <ygj960518@gmail.com>
2024-03-03 20:59:56 +0900

    🔨 fix(week13): 자물쇠와 열쇠

--- 747Socker/week13/자물쇠와열쇠.java
+++ 747Socker/week13/자물쇠와열쇠.java
@@ -1,61 +1,73 @@
 package jechul;
 
 import java.util.*;
+
 class 자물쇠와열쇠 {
-    
-	static class Point{
-		int x;
-		int y;
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-			
-		}
-	}
-    
-    static private boolean check(int[][] key, int[][] lock, ArrayList<Point> zeros) {
-        
-        return false;
+
+    static class Point {
+        int x, y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-    
-    static private void move() {
-    	for (int i = 0; i < N; i++) {
+
+    static private boolean check(int[][] key, int[][] lock, int startX, int startY, int M, int N) {
+        int[][] lockCopy = new int[M][M];
+
+        for (int i = 0; i < M; i++) {
+            System.arraycopy(lock[i], 0, lockCopy[i], 0, M);
+        }
+
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                
+                int lockX = i + startX;
+                int lockY = j + startY;
+                if (lockX >= 0 && lockX < M && lockY >= 0 && lockY < M) {
+                    lockCopy[lockX][lockY] += key[i][j];
+                }
             }
         }
+
+        for (int[] row : lockCopy) {
+            for (int r : row) {
+                if (r != 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
+    static private int[][] rotateKey(int[][] key) {
+        int N = key.length;
+        int[][] rotated = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                rotated[j][N - 1 - i] = key[i][j];
+            }
+        }
+        return rotated;
+    }
+
     static int N;
     public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
         N = key.length;
-        int M = lock[0].length;
-        ArrayList<Point> zeros = new ArrayList<>();
-        for(int i=0; i<M; i++) {
-        	for(int j=0; j<N; j++	) {
-        		if(lock[i][j]==0) zeros.add(new Point(i,j));
-        	}
-        }
-       
-        int[][] temp = new int[key.length][key[0].length];
-        
-        for(int k=0; k<4; k++){
-            for(int i=0; i<key.length;i++){
-                for(int j=0; j<key[0].length;j++){
-                     if (k == 0) { 
-                        temp[i][j] = key[N - j - 1][i];
-                    } else if (k == 1) { 
-                        temp[i][j] = key[N - i - 1][N - j - 1];
-                    } else if (k == 2) { 
-                        temp[i][j] = key[j][N - i - 1];
-                    } else if (k == 3) { 
-                        temp[i][j] = key[i][j];
+        int M = lock.length;
+
+        for (int rotation = 0; rotation < 4; rotation++) {
+            if (rotation > 0) {
+                key = rotateKey(key);
+            }
+
+            for (int x = -N + 1; x < M; x++) {
+                for (int y = -N + 1; y < M; y++) {
+                    if (check(key, lock, x, y, M, N)) {
+                        return true;
                     }
                 }
             }
         }
-            
-        
-        return answer;
+        return false; 
     }
 }
