a3cceb4b8305c70c6f7c8971069bd7dc16726298
damdam6 <dambi626626@gmail.com>
2024-03-18 00:14:05 +0900

    🔨 fix(week14): 추석 트래픽 수정 - minute오류 수정

--- damdam6/week14/추석 트래픽.java
+++ damdam6/week14/추석 트래픽.java
@@ -5,24 +5,32 @@ class Solution {
        
         ArrayList<traffic> tArr = new ArrayList<>();
         
-        for(int i=0;i<lines.length;i++){
+        for(int i=0; i<lines.length; i++){
             int hour = Integer.parseInt(lines[i].substring(11,13));
             int minute = Integer.parseInt(lines[i].substring(14,16));
             int second = Integer.parseInt(lines[i].substring(17,19));
             int milisecond = Integer.parseInt(lines[i].substring(20,23));
-            int end = hour * 3600*1000 + minute * 60*1000+minute*1000+milisecond;
+            int end = hour * 3600*1000 + minute * 60*1000 + second*1000 + milisecond;
             int time = (int)(Double.parseDouble(lines[i].substring(24, lines[i].length() - 1)) * 1000);
             
-            //System.out.println(time);
-            tArr.add(new traffic(end-time+1,end));
-           // System.out.println(end);
+            tArr.add(new traffic(end - time + 1, end));
         }
-        
- 
         
         int answer = 0;
         
-        
+        for(int i = 0; i < tArr.size(); i++) {
+            int count = 0;
+            int startRange = tArr.get(i).end;
+            int endRange = startRange + 1000;
+            
+            for(int j = 0; j < tArr.size(); j++){
+                if(tArr.get(j).start < endRange && tArr.get(j).end >= startRange){
+                    count++;
+                }
+            }
+            
+            answer = Math.max(answer, count);
+        }
         
         return answer;
     }
