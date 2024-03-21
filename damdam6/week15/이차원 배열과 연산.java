085a16c6774a544b6edeb552393a1c9bbc3424d5
damdam6 <dambi626626@gmail.com>
2024-03-22 00:13:41 +0900

    🔨 fix(week15): 오류 수정
    일부 오류 수정
    전체 코드 재빌드
    java 11버전에 맞춘 연산자 활용

--- damdam6/week15/이차원 배열과 연산.java
+++ damdam6/week15/이차원 배열과 연산.java
@@ -2,22 +2,19 @@ package y24Mar;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baek17140 {
-
-    static int[][] map;
+    static int[][] map = new int[101][101];
+    static int r, c, k;
+    static int rowSize = 3, colSize = 3;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        map = new int[101][101];
+        r = Integer.parseInt(st.nextToken()) - 1;
+        c = Integer.parseInt(st.nextToken()) - 1;
+        k = Integer.parseInt(st.nextToken());
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(bf.readLine());
             for (int j = 0; j < 3; j++) {
@@ -25,47 +22,71 @@ public class Baek17140 {
             }
         }
 
-        int xL = 3;
-        int yL = 3;
-
-
+        System.out.println(simulate());
     }
 
-    static public class Cnt {
-        int num;
-        int count;
-
-        Cnt(int num, int count) {
-            this.count = count;
-            this.num = num;
+    static int simulate() {
+        for (int time = 0; time <= 100; time++) {
+            if (r < rowSize && c < colSize && map[r][c] == k) return time;
+            if (rowSize >= colSize) rOperation();
+            else cOperation();
         }
+        return -1;
     }
 
-    static void R(int key, int yL) {
-        PriorityQueue<Cnt> pq = new PriorityQueue<>();
-        Map<Integer, Integer> cntMap = new HashMap<>();
-
-        for (int i = 1; i <= yL; i++) {
-            if (map[key][i] == 0) continue;
-            cntMap.compute(map[key][i], (num, count) -> count == null ? 1 : count + 1);
+    static void rOperation() {
+        int newRowSize = 0;
+        for (int i = 0; i < rowSize; i++) {
+            Map<Integer, Integer> countMap = new HashMap<>();
+            for (int j = 0; j < colSize; j++) {
+                int num = map[i][j];
+                if (num == 0) continue;
+                countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            }
+            List<int[]> nums = new ArrayList<>();
+            for (var entry : countMap.entrySet()) {
+                nums.add(new int[]{entry.getKey(), entry.getValue()});
+            }
+            nums.sort((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+            int idx = 0;
+            for (int[] num : nums) {
+                if (idx == 100) break; // 최대 크기 제한
+                map[i][idx++] = num[0];
+                map[i][idx++] = num[1];
+            }
+            newRowSize = Math.max(newRowSize, idx);
+            for (; idx < colSize; idx++) {
+                map[i][idx] = 0;
+            }
         }
-
-        cntMap.forEach((k, v) -> pq.add(new Cnt(k, v)));
-
-        int i = 1;
-        while (!pq.isEmpty()) {
-            Cnt p = pq.poll();
-            map[key][i++] = p.num;
-            map[key][i++] = p.count;
-        }
-
-        yL = Math.max(yL, i);
-
-        while (i <= 99) {
-            map[key][i++] = 0;
-            map[key][i++] = 0;
-        }
+        colSize = newRowSize;
     }
 
-
+    static void cOperation() {
+        int newColSize = 0;
+        for (int j = 0; j < colSize; j++) {
+            Map<Integer, Integer> countMap = new HashMap<>();
+            for (int i = 0; i < rowSize; i++) {
+                int num = map[i][j];
+                if (num == 0) continue;
+                countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            }
+            List<int[]> nums = new ArrayList<>();
+            for (var entry : countMap.entrySet()) {
+                nums.add(new int[]{entry.getKey(), entry.getValue()});
+            }
+            nums.sort((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+            int idx = 0;
+            for (int[] num : nums) {
+                if (idx == 100) break; // 최대 크기 제한
+                map[idx++][j] = num[0];
+                map[idx++][j] = num[1];
+            }
+            newColSize = Math.max(newColSize, idx);
+            for (; idx < rowSize; idx++) {
+                map[idx][j] = 0;
+            }
+        }
+        rowSize = newColSize;
+    }
 }
